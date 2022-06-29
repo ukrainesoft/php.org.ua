024b966e14e3e33eb7112fbd7e0b9fe460eba1f0
lis-dev <mathfak@gmail.com>
2022-06-29 19:29:23 +0300

    #3 update header

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,4 +1,22 @@
+import { CMS_WESITE_NAME } from '../lib/constants'
 import SearchBar from './searchbar'
+
+type Link = {
+  title: string
+  href: string
+}
+
+// TODO Move to l10n
+const links: Link[] = [
+  {
+    title: 'Документація',
+    href: '/index.md',
+  },
+  {
+    title: 'Про проєкт',
+    href: 'https://github.com/ukrainesoft/php.org.ua',
+  },
+]
 
 export default () => (
   <header className="flex items-center justify-between py-10 h-5 border-b border-indigo-600">
@@ -7,17 +25,15 @@ export default () => (
       className="text-2xl font-semibold block"
       href="/"
     >
-      Php.org.ua
+      {CMS_WESITE_NAME}
     </a>
-    <a className="text-gray-900 dark:text-gray-100 sm:p-4" href="/">
-      Документація
-    </a>
-    <a
-      className="text-gray-900 dark:text-gray-100 sm:p-4"
-      href="https://github.com/ukrainesoft/php.org.ua"
-    >
-      Про проєкт
-    </a>
+
+    {links.map(({ title, href }) => (
+      <a className="text-gray-900 dark:text-gray-100 sm:p-4" href={href}>
+        {title}
+      </a>
+    ))}
+
     <SearchBar />
   </header>
 )

6d3f75f215dfea74996429fe6fae53fc932ec961
lis-dev <mathfak@gmail.com>
2022-07-11 23:19:30 +0300

    deploy

---
+++ _next/static/chunks/pages/manual/uk/[slug]-f5cf399c3cac85f5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[533],{3568:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/manual/uk/[slug]",function(){return _(1812)}])}},function(n){n.O(0,[851,812,774,888,179],(function(){return u=3568,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
